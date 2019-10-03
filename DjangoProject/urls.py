0bf350c7eab8b6190a40e667f7646ff9e9398410
luangjohn <athip.si.60@ubu.ac.th>
2019-10-03 11:53:23 +0700

    new project

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from commathweb import views
+from myapp import views
 
 urlpatterns = [
     path('', views.decto32fp),
